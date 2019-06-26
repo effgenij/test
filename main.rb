75da608f12990136dae0142959b9dd6789ab5720
effgenij <effgenij@gmail.com>
2019-06-26 16:14:08 +0300

    initial commit

---
+++ main.rb
@@ -0,0 +1,22 @@
+# Оценка уровня общительности (тест В.Ф. Ряховского)
+require_relative 'test'
+require_relative 'result_printer'
+name = ARGV[0]
+
+name = 'Инкогнито' if name.nil?
+
+puts "Доброго времени суток, #{name}. Ответьте на вопросы."
+
+test = Test.new
+result = ResultPrinter.new(name)
+until test.complited?
+  puts test.ask
+  user_input = nil
+  until test.user_input_valid?(user_input)
+    puts 'Введите: 1 если ваш ответ да, или 2 если нет, или 3 если иногда и нажмите Ввод'
+    user_input = STDIN.gets.to_i
+  end
+  test.next_qestion(user_input)
+end
+
+result.print_status(test)
